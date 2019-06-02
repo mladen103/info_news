258fe9dbd9cfee2f848b05caf70273d505713008
mladja97 <djomla544@gmail.com>
2019-06-02 10:09:05 +0200

    Application, Added ICommand, EntityNotFoundException

---
+++ Application/Interfaces/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    // for insert, update, delete.. just request parameter
+    public interface ICommand<TRequest>
+    {
+        void Execute(TRequest request);
+    }
+
+    // for get and get all.. request and response parameters
+    public interface ICommand<TRequest, TResponse>
+    {
+        TResponse Execute(TRequest request);
+    }
+}
