d44a131fbed70d800c3d6254ab0776d6a44b8f13
mladja97 <djomla544@gmail.com>
2019-06-09 13:54:04 +0200

    web app init

---
+++ WebApp/Controllers/JournalistsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Commands.Journalists;
+using Application.DataTransferObjects;
+using Application.Exceptions;
+using Application.Searches;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class JournalistsController : Controller
+    {
+        private readonly IDeleteJournalistCommand deleteJournalistCommand;
+        private readonly IGetJournalistCommand getJournalistCommand;
+        private readonly IGetJournalistsCommand getJournalistsCommand;
+        private readonly IInsertJournalistCommand insertJournalistCommand;
+        private readonly IUpdateJournalistCommand updateJournalistCommand;
+
+        public JournalistsController(IDeleteJournalistCommand deleteJournalistCommand, IGetJournalistCommand getJournalistCommand, IGetJournalistsCommand getJournalistsCommand, IInsertJournalistCommand insertJournalistCommand, IUpdateJournalistCommand updateJournalistCommand)
+        {
+            this.deleteJournalistCommand = deleteJournalistCommand;
+            this.getJournalistCommand = getJournalistCommand;
+            this.getJournalistsCommand = getJournalistsCommand;
+            this.insertJournalistCommand = insertJournalistCommand;
+            this.updateJournalistCommand = updateJournalistCommand;
+        }
+
+        // GET: Journalists
+        public IActionResult Index(JournalistSearch search)
+        {
+            var journalists = this.getJournalistsCommand.Execute(search);
+            return View(journalists);
+        }
+
+        // GET: Journalists/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var journalist = this.getJournalistCommand.Execute(id);
+                return View(journalist);
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        // GET: Journalists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Journalists/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(JournalistDto journalist)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+
+                try
+                {
+                    this.insertJournalistCommand.Execute(journalist);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception)
+                {
+                    TempData["error"] = "An error has occured.";
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Journalists/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var journalist = this.getJournalistCommand.Execute(id);
+                return View(journalist);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return RedirectToAction("index");
+            }
+        }
+
+        // POST: Journalists/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, JournalistDto journalist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(journalist);
+            }
+
+            try
+            {
+                this.updateJournalistCommand.Execute(journalist);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityNotFoundException)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityAlreadyExistsException)
+            {
+                TempData["error"] = "Journalist already exists.";
+                return View(journalist);
+            }
+        }
+        
+        // POST: Journalists/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                this.deleteJournalistCommand.Execute(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Application.Commands.Journalists;
+using DataAccess;
+using EFCommands;
+using EFCommands.Journalists;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<Context>();
+
+            services.AddTransient<IDeleteJournalistCommand, EfDeleteJournalistCommand>();
+            services.AddTransient<IGetJournalistCommand, EfGetJournalistCommand>();
+            services.AddTransient<IGetJournalistsCommand, EfGetJournalistsCommand>();
+            services.AddTransient<IInsertJournalistCommand, EfInsertJournalistCommand>();
+            services.AddTransient<IUpdateJournalistCommand, EfUpdateJournalistCommand>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
